071f90406a65b3e04c479d755d6908378e132906
Piyush Gour <piyushgour2301@gmail.com>
2023-09-30 23:25:20 +0530

    File Handling Commit

--- DapperApp/Controllers/UploadController.cs
+++ DapperApp/Controllers/UploadController.cs
@@ -1,4 +1,5 @@
-﻿using DapperApp.Models;
+﻿using Azure;
+using DapperApp.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -99,7 +100,7 @@ namespace DapperApp.Controllers
                 string imagepath = Filepath + "\\" + imagecode + ".jpg";
                 if (System.IO.File.Exists(imagepath))
                 {
-                    ImageURL = hostURL + "/Upload/image4/" + imagecode + ".jpg";
+                    ImageURL = hostURL + "/Upload/image/" + imagecode + "/" + imagecode + ".jpg";
                 }
                 else
                 {
@@ -114,10 +115,101 @@ namespace DapperApp.Controllers
         }
 
 
+        [HttpGet("GetMultipleImage")]
+        public async Task<IActionResult> GetMultipleImage(string imagecode)
+        {
+            List<string> ImageURL = new List<string>();
+            string hostURL = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
+            try
+            {
+                string Filepath = GetFilePath(imagecode);
+                if(System.IO.Directory.Exists(Filepath))
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(Filepath);
+                    FileInfo[] fileInfos = directoryInfo.GetFiles();
+                    foreach (FileInfo fileInfo in fileInfos)
+                    {
+                        string fileName = fileInfo.Name;
+                        string imagepath = Filepath + "\\" + fileName;
+                        if (System.IO.File.Exists(imagepath))
+                        {
+                            string _imageURL = hostURL + "/Upload/image/" + imagecode + "/" + fileName;
+                            ImageURL.Add(_imageURL);
+                        }                
+                    }
+                }              
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return Ok(ImageURL);
+        }
+
+
+
+        [HttpGet("DownloadImage")]
+        public async Task<IActionResult> DownloadImage(string imagecode)
+        {
+            try
+            {
+                string Filepath = GetFilePath(imagecode);
+                string imagepath = Filepath + "\\" + imagecode + ".jpg";
+                if (System.IO.File.Exists(imagepath))
+                {
+                    MemoryStream stream = new MemoryStream();
+                    using (FileStream fileStream = new FileStream(imagepath, FileMode.Open))
+                    {
+                        await fileStream.CopyToAsync(stream);
+                    }
+                    stream.Position = 0;
+                    return File(stream, "image/png", imagecode + ".png");   //(stream, type (jpg/png), filename
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpDelete("RemoveImage")]
+        public async Task<IActionResult> RemoveImage(string imagecode)
+        {
+            try
+            {
+                string Filepath = GetFilePath(imagecode);
+                string imagepath = Filepath + "\\" + imagecode + ".jpg";
+                if (System.IO.File.Exists(imagepath))
+                {
+                    System.IO.File.Delete(imagepath);
+                    return Ok("pass");
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+
+
         [NonAction]
         private string GetFilePath(string imagecode)
         {
-            return _webHostenvironment.WebRootPath + "\\Upload\\image" + imagecode;
+            return _webHostenvironment.WebRootPath + "\\Upload\\image\\" + imagecode;
         }
     }
  }
